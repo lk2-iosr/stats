1dc5f2de98df685a580892627a8473b95a08ffd9
Katarzyna <kmakonieczna@gmail.com>
2018-01-26 16:17:17 +0100

    fix logging

--- src/main/java/iosr/facebookapp/stats/logging/Log.java
+++ src/main/java/iosr/facebookapp/stats/logging/Log.java
@@ -3,11 +3,17 @@ package iosr.facebookapp.stats.logging;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Log {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public static String getLog(Object logObject) throws JsonProcessingException {
-        return OBJECT_MAPPER.writeValueAsString(logObject);
+    public static String getLog(TimeLog timeLog) throws JsonProcessingException{
+        Map<String, Object> map = new HashMap<>();
+        map.put(timeLog.getEventType(), timeLog.getTime());
+
+        return OBJECT_MAPPER.writeValueAsString(map);
     }
 }
